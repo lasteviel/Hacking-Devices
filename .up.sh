326ea8849a4527bc1f730cf1940bc6e376733f7d
lasteviel <47839503+lasteviel@users.noreply.github.com>
2019-02-21 05:03:38 +0200

    Add files via upload

---
+++ .up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $Zoal -eq 77 ]
+then
+cd $HOME
+rm -rf Hack-Devices
+echo -e "git clone https://github.com/saeedo201/MetaSploit"
+clear
+echo -e $cayn #---------->Hi New Update<----------#
+sleep 2
+cd $HOME
+fi
---
+++ README.md
@@ -0,0 +1,19 @@
+This Tool his helpful for Hacking 
+
+
+Don't Copy this code without give me the credit, nerd!
+
+
+
+
+#usage
+
+cd Hack-Devices 
+
+ls
+
+
+chmod +x hacking.sh
+
+
+bash hacking.sh
